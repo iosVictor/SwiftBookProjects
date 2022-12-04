1754e2900429971f4b2510c20fbc7e20d30c0a41
victorKimpel() <mars.2874@gmail.com>
2022-12-04 23:48:57 +0300

    04.12.2022

---
+++ Collection-based applications/EmojiReader/EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Victor Kimpel on 4.12.22.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
