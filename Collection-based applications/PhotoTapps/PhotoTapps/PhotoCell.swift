d334b5e6f30c4ee1d14b27d452910028a9704edb
victorKimpel() <mars.2874@gmail.com>
2022-12-07 17:10:29 +0300

    07.12.2022

---
+++ Collection-based applications/PhotoTapps/PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Victor Kimpel on 5.12.22.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dogImageView: UIImageView!
+}
