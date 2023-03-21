8dad9cef9fdeb7a07e0912090501b561e15fd401
victorKimpel() <mars.2874@gmail.com>
2023-03-21 21:55:16 +0300

    Custom Table View Cell

---
+++ Приложения на основе UITableView/MyPlaces/MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Victor Kimpel on 21.03.23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
