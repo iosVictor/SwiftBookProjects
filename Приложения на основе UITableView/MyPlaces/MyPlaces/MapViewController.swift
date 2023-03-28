796bbfe94ed378999c60870990fb970406893d32
victorKimpel() <mars.2874@gmail.com>
2023-03-28 18:19:51 +0300

    add map view controller

---
+++ Приложения на основе UITableView/MyPlaces/MyPlaces/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Victor Kimpel on 28.03.23.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+
+}
