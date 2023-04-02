f4db4c6c7e95eb5763dabc3ce835d2628a0179ef
victorKimpel() <mars.2874@gmail.com>
2023-04-03 00:50:49 +0300

    save and delete data

---
+++ CoreData basics/MealTime/CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Victor Kimpel on 1.04.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
---
+++ CoreData basics/MealTime/CoreData Entities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Victor Kimpel on 1.04.23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
