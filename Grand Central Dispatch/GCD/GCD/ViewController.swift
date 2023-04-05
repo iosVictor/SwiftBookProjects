7ecf386675a36dc11b63f3e217476300ece9a919
victorKimpel() <mars.2874@gmail.com>
2023-04-05 17:59:32 +0300

    add gif file

---
+++ Grand Central Dispatch/GCD/GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Victor Kimpel on 5.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
