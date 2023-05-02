b583d4a31e51fc5b074dc6ab2aeb91bb5e782474
victorKimpel() <mars.2874@gmail.com>
2023-05-02 23:15:07 +0300

    cleaning our project

--- SpriteKit/WarFly/WarFly/Island.swift
+++ SpriteKit/WarFly/WarFly/Island.swift
@@ -9,14 +9,16 @@ import SpriteKit
 import GameplayKit
 
 final class Island: SKSpriteNode, GameBackgroundSpriteable {
-    static func populateSprite(at point: CGPoint) -> Island {
+    static func populate(at point: CGPoint?) -> Island {
         let islandImageName = configureName()
         let island = Island(imageNamed: islandImageName)
         island.setScale(randomScaleFactor)
-        island.position = point
+        island.position = point ?? randomPoint()
         island.zPosition = 1
+        island.name = "backgroundSprite"
+        island.anchorPoint = CGPoint(x: 0.5, y: 1.0)
         island.run(rotateForRandomAngle())
-        island.run(move(from: point))
+        island.run(move(from: island.position))
         return island
     }
     
@@ -45,7 +47,7 @@ final class Island: SKSpriteNode, GameBackgroundSpriteable {
     fileprivate static func move(from point: CGPoint) -> SKAction {
         let movePoint = CGPoint(x: point.x, y: -200)
         let moveDistance = point.y + 200
-        let movementSpeed: CGFloat = 10.0
+        let movementSpeed: CGFloat = 100.0
         let duration = moveDistance / movementSpeed
         return SKAction.move(to: movePoint, duration: TimeInterval(duration))
     }
---
+++ SpriteKit/WarFly/WarFly/protocol GameBackgroundSpriteable + Extension.swift
@@ -0,0 +1,24 @@
+//
+//  protocol GameBackgroundSpriteable + Extension.swift
+//  WarFly
+//
+//  Created by Victor Kimpel on 27.04.23.
+//
+
+import SpriteKit
+import GameplayKit
+
+protocol GameBackgroundSpriteable {
+    static func populate(at point: CGPoint?) -> Self
+    static func randomPoint() -> CGPoint
+}
+
+extension GameBackgroundSpriteable {
+    static func randomPoint() -> CGPoint {
+        let screen = UIScreen.main.bounds
+        let distribution = GKRandomDistribution(lowestValue: Int(screen.size.height) + 400, highestValue: Int(screen.size.height) + 500)
+        let y = CGFloat(distribution.nextInt())
+        let x = CGFloat(GKRandomSource.sharedRandom().nextInt(upperBound: Int(screen.size.width)))
+        return CGPoint(x: x, y: y)
+    }
+}
