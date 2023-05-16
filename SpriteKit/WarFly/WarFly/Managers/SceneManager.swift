77a53ed887f46742d4b8c91b55396407e7ab5a03
victorKimpel() <mars.2874@gmail.com>
2023-05-16 23:04:09 +0300

    Scene manager

---
+++ SpriteKit/WarFly/WarFly/Managers/SceneManager.swift
@@ -0,0 +1,12 @@
+//
+//  SceneManager.swift
+//  WarFly
+//
+//  Created by Victor Kimpel on 16.05.23.
+//
+
+class SceneManager {
+    static let shared = SceneManager()
+    
+    var gameScene: GameScene?
+}
