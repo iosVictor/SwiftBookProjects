83a53437a447d66849355c16d488ab81e6751675
victorKimpel() <mars.2874@gmail.com>
2023-05-16 18:10:39 +0300

    Pause scene

--- SpriteKit/WarFly/WarFly/Scenes/GameScene.swift
+++ SpriteKit/WarFly/WarFly/Scenes/GameScene.swift
@@ -10,14 +10,10 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var player: PlayerPlane!
-    let scoreBackground = SKSpriteNode(imageNamed: "scores")
-    let scoreLabel = SKLabelNode(text: "10000")
-    let menuButton = SKSpriteNode(imageNamed: "menu")
-    let life1 = SKSpriteNode(imageNamed: "life")
-    let life2 = SKSpriteNode(imageNamed: "life")
-    let life3 = SKSpriteNode(imageNamed: "life")
- 
+    fileprivate var player: PlayerPlane!
+    fileprivate let hud = HUD()
+    fileprivate let screenSize = UIScreen.main.bounds.size
+    
     override func didMove(to view: SKView) {
         
         physicsWorld.contactDelegate = self
@@ -30,35 +26,12 @@ class GameScene: SKScene {
         
         spawnPowerUp()
         spawnEnemies()
-        configureUI()
+        createHUD()
     }
     
-    fileprivate func configureUI() {
-        scoreBackground.position = CGPoint(x: scoreBackground.size.width + 10, y: self.size.height - scoreBackground.size.height / 2 - 10)
-        scoreBackground.anchorPoint = CGPoint(x: 1.0, y: 0.5)
-        scoreBackground.zPosition = 99
-        addChild(scoreBackground)
-        
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.verticalAlignmentMode = .center
-        scoreLabel.position = CGPoint(x: -10, y: 3)
-        scoreLabel.zPosition = 100
-        scoreLabel.fontName = "AmericanTypewriter-Bold"
-        scoreLabel.fontSize = 30
-        scoreBackground.addChild(scoreLabel)
-        
-        menuButton.position = CGPoint(x: 20, y: 20)
-        menuButton.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-        menuButton.zPosition = 100
-        addChild(menuButton)
-        
-        let lifes = [life1, life2, life3]
-        for (index, life) in lifes.enumerated() {
-            life.position = CGPoint(x: self.size.width - CGFloat(index + 1) * (life.size.width + 3), y: 20)
-            life.zPosition = 100
-            life.anchorPoint = CGPoint(x: 0.0, y: 0.0)
-            addChild(life)
-        }
+    fileprivate func createHUD() {
+        addChild(hud)
+        hud.configureUI(screenSize: screenSize)
     }
     
     fileprivate func spawnPowerUp() {
@@ -179,7 +152,17 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        playerFire()
+        let location = touches.first!.location(in: self)
+        let node = self.atPoint(location)
+        
+        if node.name == "pause" {
+            let transition = SKTransition.doorway(withDuration: 1.0)
+            let pauseScene = PauseScene(size: self.size)
+            pauseScene.scaleMode = .aspectFill
+            self.scene!.view?.presentScene(pauseScene, transition: transition)
+        } else {
+            playerFire()
+        }
     }
 }
 
---
+++ SpriteKit/WarFly/WarFly/Scenes/MenuScene.swift
@@ -0,0 +1,45 @@
+//
+//  MenuScene.swift
+//  WarFly
+//
+//  Created by Victor Kimpel on 8.05.23.
+//
+
+import SpriteKit
+
+class MenuScene: SKScene {
+    override func didMove(to view: SKView) {
+        if !Assets.shared.isLoaded {
+            Assets.shared.preloadAssets()
+            Assets.shared.isLoaded = true
+        }
+        
+        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
+        
+        let header = SKSpriteNode(imageNamed: "header1")
+        header.position = CGPoint(x: self.frame.midX, y: self.frame.midY + 150)
+        self.addChild(header)
+        
+        let titles = ["play", "options", "best"]
+        
+        for (index, title) in titles.enumerated() {
+            let button = ButtonNode(titled: title, backgroundName: "button_background")
+            button.position = CGPoint(x: self.frame.midX, y: self.frame.midY - CGFloat(100 * index))
+            button.name = title
+            button.label.name = title
+            addChild(button)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let location = touches.first!.location(in: self)
+        let node = self.atPoint(location)
+        
+        if node.name == "play" {
+            let transition = SKTransition.crossFade(withDuration: 1.0)
+            let gameScene = GameScene(size: self.size)
+            gameScene.scaleMode = .aspectFill
+            self.scene!.view?.presentScene(gameScene, transition: transition)
+        }
+    }
+}
