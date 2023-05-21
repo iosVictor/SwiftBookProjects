cfe8fb2fbd512d1f31ad1413e11ee1742053b005
victorKimpel() <mars.2874@gmail.com>
2023-05-21 19:14:13 +0300

    Best scene

--- SpriteKit/WarFly/WarFly/Scenes/OptionsScene.swift
+++ SpriteKit/WarFly/WarFly/Scenes/OptionsScene.swift
@@ -10,7 +10,6 @@ import SpriteKit
 class OptionsScene: ParentScene {
     
     override func didMove(to view: SKView) {
-        self.backgroundColor = SKColor(red: 0.15, green: 0.15, blue: 0.3, alpha: 1.0)
         
         setHeader(withName: "options", andBackground: "header_background")
         
