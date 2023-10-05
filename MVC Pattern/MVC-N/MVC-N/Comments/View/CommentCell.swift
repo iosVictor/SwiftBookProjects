5aa57df3bd2339df2646e908543913eb86f0c49f
victorKimpel() <mars.2874@gmail.com>
2023-10-05 23:12:57 +0300

    Project: part 2

---
+++ MVC Pattern/MVC-N/MVC-N/Comments/View/CommentCell.swift
@@ -0,0 +1,19 @@
+//
+//  CommentCell.swift
+//  MVC-N
+//
+//  Created by Victor Kimpel on 5.10.23.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
+    func configure(with comment: Comment) {
+        self.label.text = comment.name
+        self.textView.text = comment.body
+    }
+}
