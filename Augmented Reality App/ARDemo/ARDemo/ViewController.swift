28f49feba99f3302b0d8e75901c73648997f5819
victorKimpel() <mars.2874@gmail.com>
2023-10-18 22:54:13 +0300

    Plane class

--- Augmented Reality App/ARDemo/ARDemo/ViewController.swift
+++ Augmented Reality App/ARDemo/ARDemo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
     var session: ARSession {
@@ -19,28 +19,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        sceneView.delegate = self
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        sceneView.autoenablesDefaultLighting = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         
         let scene = SCNScene()
         
         sceneView.scene = scene
-    }
-        
-    private func createBox(in scene: SCNScene) {
-        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
-        let boxMaterial = SCNMaterial()
-        
-        boxMaterial.diffuse.contents = UIColor.red
-        boxMaterial.specular.contents = UIColor.yellow
-        
-        let boxNode = SCNNode(geometry: box)
-        boxNode.name = "box"
-        boxNode.geometry?.materials = [boxMaterial]
-        boxNode.position = SCNVector3(0.0, 0.0, -0.5)
-        scene.rootNode.addChildNode(boxNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,21 +44,4 @@ class ViewController: UIViewController {
         // Pause the view's session
         sceneView.session.pause()
     }
-    
-    @IBAction func resetTapped(sender: UIButton) {
-        session.pause()
-        sceneView.scene.rootNode.enumerateChildNodes { node, _ in
-            if node.name == "box" {
-                node.removeFromParentNode()
-            }
-        }
-        
-        let configuration = ARWorldTrackingConfiguration()
-        session.run(configuration, options: [.removeExistingAnchors, .resetTracking])
-    }
-    
-    @IBAction func addTapped(sender: UIButton) {
-        createBox(in: sceneView.scene)
-    }
-    
 }
