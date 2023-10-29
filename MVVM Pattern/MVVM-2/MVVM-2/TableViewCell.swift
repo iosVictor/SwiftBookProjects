6fc860c6384a2e60d28c3b80130460e9dc7d1405
victorKimpel() <mars.2874@gmail.com>
2023-10-29 13:39:05 +0300

    UITableView with MVC

---
+++ MVVM Pattern/MVVM-2/MVVM-2/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Victor Kimpel on 29.10.23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+
+}
