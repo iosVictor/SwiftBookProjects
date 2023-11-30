f89ad459a20b55986f46bfa81a828b8acaa256cd
victorKimpel() <mars.2874@gmail.com>
2023-11-30 20:55:27 +0300

    запрос авторизации

---
+++ Push-уведомления/Notifications/Notifications/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  Notifications
+//
+//  Created by Victor Kimpel on 30.11.2023.
+//  Copyright © 2023 Victor Kimpel. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    let notificationCenter = UNUserNotificationCenter.current()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        requestAutorisation()
+        return true
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+    
+    func requestAutorisation() {
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            print("Permission granted: \(granted)")
+            
+            guard granted else { return }
+            self.getNotificationSettings()
+        }
+    }
+    
+    func getNotificationSettings() {
+        notificationCenter.getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+        }
+    }
+}
+
