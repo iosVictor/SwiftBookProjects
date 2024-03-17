05612ec613a3148adda40a86cf98efb2dc242fba
victorKimpel() <mars.2874@gmail.com>
2024-03-17 14:37:57 +0300

    загрузка изображения из сети

---
+++ Networking/Networking/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Victor Kimpel on 17/03/2024.
+//  Copyright © 2024 Victor Kimpel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
