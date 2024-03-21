ec062586cde38138b7426eb40c560372eaef7d74
victorKimpel() <mars.2874@gmail.com>
2024-03-21 13:13:22 +0300

    Download Progress

---
+++ Networking/Networking_6/Networking/AlamofireNetworkRequest.swift
@@ -0,0 +1,118 @@
+//
+//  AlamofireNetworkRequest.swift
+//  Networking
+//
+//  Created by Victor Kimpel on 21/03/2024.
+//  Copyright © 2024 Victor Kimpel. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AlamofireNetworkRequest {
+    
+    static var onProgress: ((Double) -> ())?
+    static var completed: ((String) -> ())?
+    
+    static func sendRequest(url: String, completion: @escaping (_ courses: [Course])->()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        
+        request(url, method: .get).validate().responseJSON { (response) in
+            
+            switch response.result {
+                
+            case .success(let value):
+                
+                var courses = [Course]()
+                courses = Course.getArray(from: value)!
+                completion(courses)
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func downloadImage(url: String, completion: @escaping (_ image: UIImage)->()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        request(url).responseData { (responseData) in
+            
+            switch responseData.result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else { return }
+                completion(image)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func responseData(url: String) {
+        
+        request(url).responseData { (responseData) in
+            
+            switch responseData.result {
+            case .success(let data):
+                guard let string = String(data: data, encoding: .utf8) else { return }
+                print(string)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func responseString(url: String) {
+        
+        request(url).responseString { (responseString) in
+            
+            switch responseString.result {
+            case .success(let string):
+                print(string)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func response(url: String) {
+        
+        request(url).response { (response) in
+            
+            guard
+                let data = response.data,
+                let string = String(data: data, encoding: .utf8)
+                else { return }
+            
+            print(string)
+        }
+    }
+    
+    static func downloadImageWithProgress(url: String, completion: @escaping (_ image: UIImage) -> ()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        request(url).validate().downloadProgress { progress in
+            
+            print("totalUnitCount: \(progress.totalUnitCount)\n" )
+            print("completedUnitCount: \(progress.completedUnitCount)\n")
+            print("fractionCompleted: \(progress.fractionCompleted)\n")
+            print("localizedDescription: \(progress.localizedDescription)\n")
+            print("-----------------------------------------------------------")
+            
+            self.onProgress?(progress.fractionCompleted)
+            self.completed?(progress.localizedDescription)
+            
+        }.response { response in
+            
+            guard let data = response.data, let image = UIImage(data: data) else { return }
+            
+            DispatchQueue.main.async {
+                completion(image)
+            }
+        }
+    }
+}
